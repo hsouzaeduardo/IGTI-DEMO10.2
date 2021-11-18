c8e3c445002651407b90d6e3e50a28ba2ed03b3e
Henrique Eduardo Souza <hsouza.eduardo@gmail.com>
2021-11-17 22:58:13 -0300

    Inclusão novos files

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace IGTI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("TESTE GIT");
             Console.WriteLine("Hello World!");
         }
     }
